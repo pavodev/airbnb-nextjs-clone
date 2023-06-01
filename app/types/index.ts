78f1c89728d21c1ae58be4832c2cf47b654003eb
Ivan Pavic <ivan96jp@gmail.com>
2023-06-01 15:57:04 +0200

    added github login and register

---
+++ app/types/index.ts
@@ -0,0 +1,11 @@
+import { User } from "@prisma/client";
+
+// Safe user
+export type SafeUser = Omit<
+  User,
+  "createdAt" | "updatedAt" | "emailVerified"
+> & {
+  createdAt: string;
+  updatedAt: string;
+  emailVerified: string | null;
+};
